00b66e481a0062f8a373c962c1f7ee06eb6fbcf9
mkilmer <marcoskilmer1@gmail.com>
2020-05-17 11:15:57 -0300

    Initial Commit

---
+++ SOLID/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SOLID
+//
+//  Created by Marcos Kilmer on 17/05/20.
+//  Copyright © 2020 mkilmer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
